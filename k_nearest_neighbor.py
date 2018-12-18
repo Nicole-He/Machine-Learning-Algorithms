4d2a686949b25e041b20e30a192cae555d6e36f5
Yiling He <yilinghe2016@gmail.com>
2018-12-18 12:06:18 -0600

    Initial upload

---
+++ k_nearest_neighbor.py
@@ -0,0 +1,58 @@
+import numpy as np
+
+class KNearestNeighbor(object):
+  """ a kNN classifier with L2 distance """
+
+  def __init__(self):
+    pass
+
+  def train(self, X, y):
+    """
+    Train the classifier. For k-nearest neighbors this is just 
+    memorizing the training data.
+
+    Inputs:
+    - X: A numpy array of shape (num_train, D) containing the training data
+      consisting of num_train samples each of dimension D.
+    - y: A numpy array of shape (N,) containing the training labels, where
+         y[i] is the label for X[i].
+    """
+    self.X_train = X
+    self.y_train = y
+    
+  def compute_distances(self, X):
+    """
+    Compute the distance between each test point in X and each training point
+    in self.X_train using no explicit loops.
+
+    Input / Output: Same as compute_distances_two_loops
+    """
+    num_test = X.shape[0]
+    num_train = self.X_train.shape[0]
+    dists = np.zeros((num_test, num_train))     
+    dists = np.sqrt(-2*np.dot(X, self.X_train.T) + 
+                    np.sum(self.X_train**2, axis = 1) + 
+                    np.sum(X**2, axis = 1)[:, np.newaxis])
+    return dists
+
+  def predict(self, dists, k=1):
+    """
+    Given a matrix of distances between test points and training points,
+    predict a label for each test point.
+
+    Inputs:
+    - dists: A numpy array of shape (num_test, num_train) where dists[i, j]
+      gives the distance betwen the ith test point and the jth training point.
+
+    Returns:
+    - y: A numpy array of shape (num_test,) containing predicted labels for the
+      test data, where y[i] is the predicted label for the test point X[i].  
+    """
+    num_test = dists.shape[0]
+    y_pred = np.zeros(num_test)
+    for i in range(num_test):
+        closest_y = []
+        closest_y = self.y_train[np.argsort(dists[i])[:k]]
+        y_pred[i] = np.bincount(closest_y).argmax()
+    return y_pred
+
